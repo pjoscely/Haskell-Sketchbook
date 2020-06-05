798ebc4536f231416033951ac584a5ce69204349
Charles Joscelyne <pjoscely@hotmail.com>
2020-06-05 15:58:10 -0400

    Add files via upload

--- Haskell University of Glasgow/spellBook.hs
+++ Haskell University of Glasgow/spellBook.hs
@@ -1,1 +1,32 @@
+--Future Learn FUNCTIONAL PROGRAMMING IN HASKELL 
+--Week 3 Spell Book Generator
+--Helper function
+-- "zoo"
+--"z is for zoo"
+singleWord :: String ->String
+singleWord a = take 1 a ++" is for "++a
+
+
+-- Main Speller Fucntion
+
+--speller ["abacus"] -- > "a is for abacus"
+--speller [] -- > ""
+--speller ["apple", "banana", "coconut"] 
+-- > "a is for apple, b is for banana, and c is for coconut"
+--speller ["whisky", "x-ray"]
+-- > "w is for whisky, and x is for x-ray"
+
+--Use recursion to reduce to three pattern matches
+speller :: [String] -> String
+speller [] = ""
+speller (x:[]) = singleWord x 
+speller(x:y:[])= singleWord x ++ ", and "++singleWord y
+speller(x:xs) = singleWord x++ ", "++speller xs
+
+
+
+
+
+
+
 
