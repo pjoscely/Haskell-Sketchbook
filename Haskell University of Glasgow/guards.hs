e6d2ded8afbd2326e0c1d9273250be32e258a29e
Charles Joscelyne <pjoscely@hotmail.com>
2020-06-08 17:23:54 -0400

    Add files via upload

--- Haskell University of Glasgow/guards.hs
+++ Haskell University of Glasgow/guards.hs
@@ -13,6 +13,15 @@ holeScore strokes par
   | otherwise = show(score) ++ " over par"
  where score = strokes-par
 
+--Taken from http://learnyouahaskell.com/
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2  
+
 --Case statement
 --hello (Parrot "polly")
 data Pet = Cat | Dog | Fish | Parrot String
