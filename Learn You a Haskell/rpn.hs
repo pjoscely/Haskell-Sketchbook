db52f0096ad02287273e0d328897d87d648e5442
Charles Joscelyne <pjoscely@hotmail.com>
2020-06-09 13:51:58 -0400

    Add files via upload

---
+++ Learn You a Haskell/rpn.hs
@@ -0,0 +1,14 @@
+--Reverse Polish notation calculator
+import Data.List  
+  
+solveRPN :: String -> Float  
+solveRPN = head . foldl foldingFunction [] . words  
+    where   foldingFunction (x:y:ys) "*" = (x * y):ys  
+            foldingFunction (x:y:ys) "+" = (x + y):ys  
+            foldingFunction (x:y:ys) "-" = (y - x):ys  
+            foldingFunction (x:y:ys) "/" = (y / x):ys  
+            foldingFunction (x:y:ys) "^" = (y ** x):ys  
+            foldingFunction (x:xs) "ln" = log x:xs  
+            foldingFunction (x:xs) "sin" = sin x:xs  
+            foldingFunction xs "sum" = [sum xs]  
+            foldingFunction xs numberString = read numberString:xs  
