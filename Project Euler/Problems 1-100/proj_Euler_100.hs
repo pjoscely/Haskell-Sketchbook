c8876578ea7e29485fb02721a14cbc03baa7288e
Charles Joscelyne <pjoscely@hotmail.com>
2020-08-23 03:30:40 -0500

    Add files via upload

--- Project Euler/Problems 1-100/proj_Euler_100.hs
+++ Project Euler/Problems 1-100/proj_Euler_100.hs
@@ -45,22 +45,43 @@ print(b,n)
 import Data.List
 import Data.Maybe
 
+-- Iteration functions for b and n
+-- b is number of blue chips
+-- n is total number of chips
+fx :: Num a => a -> a -> a
 fx b n = 3 * b + 2 * n - 2
 
+fy :: Num a => a -> a -> a
 fy b n = 4 * b + 3 * n - 3
 
-
+-- Generate iteration pairs 
+-- f(15,21) -> (85,120) -> (493,697)
+f :: Num b => (b, b) -> (b, b)
 f (b, n) = (fx b n, fy b n)
 
+-- Lazy generation of list of iterates 
+-- iterate f (15, 21) -> [(15,21),(85,120),(493,697),(2871,4060),(16731,23661),...,]
 lst = iterate f (15, 21)
 
+-- Find the first n value > 10^12
+-- total -> 1070379110497
+total :: Integer
 total = head $filter (>10^12)$map snd lst
 
-indx = fromJust$elemIndex 1070379110497 (map snd$ lst)
+-- Find index of pair in lst where n = total
+-- indx -> 14
+indx :: Int
+indx = fromJust$elemIndex total (map snd$ lst)
 
+-- answer is the first coordinate at the indx
+-- fst (756872327473,1070379110497) -> 756872327473
+answer :: Integer
 answer = fst$ iterate f (15, 21)!!indx
 
+-- Display answer
+--  main -> 756872327473
+-- (0.04 secs, 156,464 bytes)
 main :: IO ()
 main = do  
     (putStrLn.show) answer
-    
+
