d9864a45b8aa14202f992ea0f5f35ba6a59f438b
Charles Joscelyne <pjoscely@hotmail.com>
2020-09-07 00:32:41 -0500

    Add files via upload

--- CIS 194: Introduction to Haskell/Homework 1/hanoi.hs
+++ CIS 194: Introduction to Haskell/Homework 1/hanoi.hs
@@ -6,12 +6,12 @@ all disks stacked on the first peg to an ending configuration with all
 disks stacked on the last peg.
 
 *       *       *
-*		    *       *
+*		*       *
 *       *       *
 *       *       *
 *       *       *
 
-a       b       c
+src    aux     dest
 
 The only rules are
 • you may only move one disk at a time, and
@@ -44,14 +44,57 @@ give shorter names to complicated types, or (as here) simply to help
 with documentation.
 
 -}
--- Example: hanoi 2 "a" "b" "c" == [("a","c"), ("a","b"), ("c","b")]
 
 type Peg = String
 type Move = (Peg, Peg)
--- Example: hanoi 2 "a" "b" "c" == [("a","c"), ("a","b"), ("c","b")]
+--Example: hanoi 2 "a" "c" "b" -> [("a","b"),("a","c"),("b","c")]
+--Example: hanoi 3 "a" "c" "b" ->
+--[("a","c"),("a","b"),("c","b"),("a","c"),("b","a"),("b","c"),("a","c")]
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 src dest aux = []
+hanoi n src dest aux = hanoi (n-1) src aux dest ++ [(src,dest)] ++ hanoi (n-1) aux dest src
+{-
+Exercise 6 (Optional) What if there are four pegs instead of three?
+That is, the goal is still to move a stack of discs from the first peg to
+the last peg, without ever placing a larger disc on top of a smaller
+one, but now there are two extra pegs that can be used as “temporary” storage instead of only one. 
+Write a function similar to hanoi
+which solves this problem in as few moves as possible.
+It should be possible to do it in far fewer moves than with three
+pegs. For example, with three pegs it takes 2^15 − 1 = 32767 moves
+to transfer 15 discs. With four pegs it can be done in 129 moves. (See
+Exercise 1.17 in Graham, Knuth, and Patashnik, Concrete Mathematics,
+second ed., Addison-Wesley, 1994.)
 
-hanoi 0 a b c = []
-hanoi n a b c = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
+The Frame-Stewart algorithm:
+1. for some k (1 <= k < n), move k discs from src to aux1
+2. move the remaining n-k discs from src to dst without using aux1
+3. move k discs from aux1 to dst
+It has been proved that for 4 pegs, the value of k that minimizes
+the number of moves is the one that we have used here.
+https://en.wikipedia.org/wiki/Tower_of_Hanoi#Frame%E2%80%93Stewart_algorithm
+-}
+-- hanoi4 3  "a" "d" "b" "c" ->
+-- [("a","b"),("a","c"),("a","d"),("c","d"),("b","d")]
+-- length$hanoi4 15  "a" "d" "b" "c" -> 129
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg->[Move]
+hanoi4 0 _ _ _ _ = []
+hanoi4 n src dst aux1 aux2 =
+       hanoi4 k src aux1 aux2 dst ++ 
+       hanoi (n-k) src dst aux2 ++ 
+       hanoi4 k aux1 dst aux2 src
+       where
+       n' = fromIntegral n :: Double
+       k  = n - round (sqrt (2*n' + 1)) + 1
+
+
+
+
+
+
+
+
+
+
 
 
