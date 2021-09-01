1a5d9a97a8c2575aed89a1807860c4a1fe34da1c
Charles Joscelyne <pjoscely@hotmail.com>
2021-09-01 11:58:50 -0500

    Update proj_Euler_004.hs

--- Project Euler/Problems 1-100/proj_Euler_004.hs
+++ Project Euler/Problems 1-100/proj_Euler_004.hs
@@ -9,12 +9,11 @@
 
 -- Find the largest palindrome made from the product of two 3-digit numbers.
 
--- One line list comprehension
+-- One line list comprehension using show, which converts a numeric value into a string.
 ans :: Integer
 ans = maximum [ (x*y)|x<- [100..999],y<- [100..999], reverse(show(x*y)) == show (x*y)] 
 
--- ans
--- 906609
+-- ans = 906609
 -- (1.18 secs, 809,169,904 bytes)
 
 -- Congratulations, the answer you gave to problem 4 is correct.
